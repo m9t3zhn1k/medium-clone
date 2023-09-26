a4a64d0a1c058229f1678263c2b98207f62317a1
Pavel Korshunau <p.korshunov@section228.com>
2023-09-26 21:33:18 +0300

    add register view styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,67 @@
 <template>
-  <p>Medium Clone</p>
-  <router-view />
+  <header class="header">
+    <p>Medium Clone</p>
+    <nav>
+      <ul class="nav-list">
+        <li class="nav-item">
+          <a class="nav-link"><router-link :to="{ name: 'home' }">Home</router-link></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link"><router-link :to="{ name: 'login' }">Sign in</router-link></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link"><router-link :to="{ name: 'register' }">Sign up</router-link></a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <div class="main">
+    <router-view />
+  </div>
+  <footer class="footer">Footer</footer>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html,
+body {
+  height: 100%;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+ul,
+ol {
+  list-style-type: none;
+}
+
+.main {
+  flex: 1 1 100%;
+}
+
+.header,
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 auto;
+  padding: 1rem;
+  max-width: 1200px;
+  width: 100%;
+}
+
+.header {
+  .nav-list {
+    display: flex;
+    gap: 1.5rem;
+  }
+}
+</style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="wrapper">
+  <div class="view-wrapper">
     <h1 class="heading">Sign Up</h1>
-    <p><router-link class :to="{ name: 'login' }">Need an accout?</router-link></p>
-    Validation errors
-    <form>
-      <input type="text" placeholder="Username" />
-      <input type="text" placeholder="Email" />
-      <input type="password" placeholder="Password" />
-      <button>Sign Up</button>
+    <a class="link"><router-link :to="{ name: 'login' }">Have an account?</router-link></a>
+    <!--  Validation errors -->
+    <form class="form">
+      <input class="input" type="text" placeholder="Username" />
+      <input class="input" type="text" placeholder="Email" />
+      <input class="input" type="password" placeholder="Password" />
+      <button class="submit">Sign Up</button>
     </form>
   </div>
 </template>
@@ -18,4 +18,79 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+.view-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  gap: 1rem;
+  padding: 2rem 1rem;
+  max-width: 1200px;
+}
+
+.heading {
+  font-size: 2.5rem;
+}
+
+.link {
+  color: #5cb85c;
+
+  &:hover {
+    text-decoration: underline;
+  }
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  max-width: 540px;
+  width: 100%;
+  gap: 1rem;
+
+  .input {
+    padding: 0.75rem 1.5rem;
+    font-size: 1.25rem;
+    border-radius: 0.3rem;
+    display: block;
+    width: 100%;
+    font-size: 1rem;
+    line-height: 1.25;
+    color: #55595c;
+    background-color: #fff;
+    background-image: none;
+    background-clip: padding-box;
+    border: 1px solid rgba(0, 0, 0, 0.15);
+    outline: none;
+
+    &:focus {
+      border: 1px solid rgba(0, 119, 255, 0.5);
+    }
+  }
+
+  .submit {
+    display: inline-block;
+    font-weight: normal;
+    line-height: 1.25;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: middle;
+    cursor: pointer;
+    user-select: none;
+    border: 1px solid transparent;
+    padding: 0.75rem 1.5rem;
+    font-size: 1.25rem;
+    border-radius: 0.3rem;
+    text-decoration: none;
+    background-color: #5cb85c;
+    border-color: #5cb85c;
+    color: #fff;
+    margin-left: auto;
+
+    &:hover {
+      background-color: #449d44;
+      border-color: #419641;
+    }
+  }
+}
+</style>
