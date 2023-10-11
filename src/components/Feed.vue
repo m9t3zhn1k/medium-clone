e9693701955346425da6a06adf01aac150a53ff6
Pavel Korshunau <p.korshunov@section228.com>
2023-10-11 22:59:22 +0300

    add feed component and home basic markup

---
+++ src/components/Feed.vue
@@ -0,0 +1,11 @@
+<template>feed</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "McvFeed",
+});
+</script>
+
+<style lang="scss"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,51 @@
 <template>
-  <p>Home page</p>
+  <p class="banner">BANNER</p>
+  <div class="wrapper">
+    <section class="content">
+      <McvFeed></McvFeed>
+      <div>POPULAR TAGS</div>
+    </section>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import McvFeed from "@/components/Feed.vue";
 
 export default defineComponent({
   name: "McvHome",
+  components: {
+    McvFeed,
+  },
 });
 </script>
+
+<style lang="scss">
+.banner {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 200px;
+  width: 100%;
+  background-color: green;
+  color: white;
+  font-size: 3rem;
+}
+
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  gap: 1rem;
+  padding: 2rem 1rem;
+  max-width: 1200px;
+}
+
+.content {
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  gap: 1rem;
+  width: 100%;
+}
+</style>
